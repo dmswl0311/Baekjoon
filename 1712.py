426ba17ef8bbdb217a38f1a91bf802405966c40b
dmswl0311 <dmswl_0311@naver.com>
2021-01-11 17:08:38 +0900

    1712 손익분기점

--- 1712.py
+++ 1712.py
@@ -1,17 +1,9 @@
-import sys
+a, b, c = map(int, input().split())
+x = 1
 
-a, b, c = map(int, sys.stdin.readline().split())
-cnt = 0
-
-if b >= c:
-    cnt = -1
+if (b >= c):
+    print("-1")
 
 else:
-    while(True):
-        cnt += 1
-        price = a+(b*cnt)
-        result_price = (c*cnt)
-        if result_price > price:
-            break
-
-print(cnt)
+    x = a//(c-b)+1
+    print(x)
