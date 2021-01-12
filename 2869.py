3ab66ee64aca8143b3a0f49a49a9335dfce742b4
dmswl0311 <dmswl_0311@naver.com>
2021-01-12 18:18:31 +0900

    2869 달팽이는 올라 싶다

--- 2869.py
+++ 2869.py
@@ -1,13 +1,26 @@
 import sys
 
-A, B, V = map(int, sys.stdin.readline().split())
-result = 0
-cnt = 0
+a, b, v = map(int, sys.stdin.readline().split())
 
-while (True):
-    result += A
-    cnt += 1
-    if (result == V):
-        print(cnt)
-        break
-    result -= B
+day = 1
+
+if (a == v):
+    print(day)
+else:
+    day = (v-a)//(a-b)
+    if (v-a) % (a-b) == 0:
+        print(day+1)
+    else:
+        print(day+2)
+
+
+# 시간초과 코드
+# x = 1
+# p = 0
+# while(True):
+#     if (p+a < v):
+#         p += a-b
+#     else:
+#         print(x)
+#         break
+#     x += 1
