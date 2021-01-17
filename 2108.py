b8fa0a61d2b6006dc267830a9507343ef4b51eb3
dmswl0311 <dmswl_0311@naver.com>
2021-01-17 20:30:59 +0900

    2108 시간초과

--- 2108.py
+++ 2108.py
@@ -2,30 +2,26 @@ import sys
 
 n = int(sys.stdin.readline())
 a = [int(sys.stdin.readline()) for _ in range(n)]
-
-print("%.f" % (round(sum(a)/n, 0)))
-a.sort()
-print(a[n//2])
 result = [0]*n
-result2 = []
+
+print("%.f" % (round(sum(a)/n, 0)))  # 산술평균
+
+a.sort()
+print(a[n//2])  # 중앙값
 
 for i in range(n):
     result[i] = (a[i], a.count(a[i]))
 
-count = 0
 result = list(set(result))
-result.sort(key=lambda x: (x[1]), reverse=True)
+result.sort(key=lambda x: (-x[1], x[0]))
 
-m = result[0][1]  # 최빈값 빈도수
+end = 0
+for i in range(1, n):
+    if (result[i][1] == result[i-1][1]):
+        end = i
+    else:
+        break
+result = result[:end+1]
 
-for i in range(n):
-    if (result[i][1] == m):
-        result2.append(result[i][0])
-        count += 1
-    elif (result[i][1] < m):
-        break
-    if count == 2:
-        break
-result2.sort()
-print(result2[-1])
-print(a[-1]-a[0])
+print(result[1][0])
+print(a[-1]-a[0])  # 범위
