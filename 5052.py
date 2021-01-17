d2ad883dc014600265c942088539152c982ac461
dmswl0311 <dmswl_0311@naver.com>
2021-01-17 20:09:43 +0900

    5052 수정

---
+++ 5052.py
@@ -0,0 +1,18 @@
+import sys
+
+t = int(sys.stdin.readline())
+result = [0]*t
+
+for i in range(t):
+    n = int(sys.stdin.readline())
+    num = [int(sys.stdin.readline().strip()) for _ in range(n)]
+
+    print(num)
+    for j in range()
+    if (num[0]):
+        result[i] = 'NO'
+    else:
+        result[i] = 'YES'
+
+for i in result:
+    print(i)
