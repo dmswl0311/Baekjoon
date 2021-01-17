88aa30c16eb1517e10ce50bbe0df6b54e7e80def
dmswl0311 <dmswl_0311@naver.com>
2021-01-17 23:25:09 +0900

    10815 숫자 카드

--- 10815.py
+++ 10815.py
@@ -6,18 +6,20 @@ m = int(sys.stdin.readline())
 b = list(map(int, sys.stdin.readline().split()))
 
 a.sort()
-result = [0]*(max(b)+1)
 
-for i in range(m):
-    for j in range(n):
-        if(b[i] == a[j]):
-            result[i] = 1
-            break
-        elif(b[i] > a[j]):
-            continue
-        else:
-            result[i] = 0
-            break
 
-for i in result:
-    print(i, end=' ')
+def binary(array, target, start, end):
+    if start > end:
+        return 0
+    mid = (start+end)//2
+
+    if array[mid] == target:
+        return 1
+    elif array[mid] > target:
+        return binary(array, target, start, mid-1)
+    else:
+        return binary(array, target, mid+1, end)
+
+
+for i in b:
+    print(binary(a, i, 0, n-1), end=' ')
