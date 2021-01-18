5ccb3274cb5330f2b93d1773487d27f02fffcb3c
dmswl0311 <dmswl_0311@naver.com>
2021-01-18 20:52:40 +0900

    1300 수정

--- 1300.py
+++ 1300.py
@@ -1,11 +1,21 @@
 n = int(input())
 k = int(input())
-b = []
 
-for i in range(n):
-    for j in range(n):
-        b.append((i+1)*(j+1))
+start = 1
+end = k
+result = 0
 
-b.sort()
+while(start <= end):
+    mid = (start+end)//2
+    cnt = 0
 
-print(b[k-1])
+    # for i in range(1, n+1):
+    #     cnt +=             모르겠음
+
+    if cnt < k:
+        start = mid+1
+    else:
+        result = mid
+        end = mid-1
+
+print(result)
