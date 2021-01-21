a6b8d12790c1bff1ff887d60719cdb5c44e2c353
dmswl0311 <dmswl_0311@naver.com>
2021-01-21 18:25:48 +0900

    1931 회의실 배정

--- 1931.py
+++ 1931.py
@@ -2,19 +2,19 @@ from sys import stdin
 
 n = int(stdin.readline())
 array = [0]*n
-result = []
+
 for i in range(n):
     start, end = map(int, stdin.readline().split())
     array[i] = (start, end)
 
-array.sort(key=lambda x: (x[0], x[1]))
-for i in range(n):
-    cnt = 1
-    next = i
-    for j in range(i+1, n):
-        if (array[next][1] <= array[j][0]):
-            next = j
-            cnt += 1
-    result.append(cnt)
+array.sort(key=lambda x: (x[1], x[0]))
 
-print(max(result))
+last = 0
+cnt = 0
+
+for i, j in array:
+    if i >= last:
+        cnt += 1
+        last = j
+
+print(cnt)
