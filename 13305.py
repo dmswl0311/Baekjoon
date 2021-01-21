b1fab4ef9dbe71693c59177ea2ca4ea0414fcb44
dmswl0311 <dmswl_0311@naver.com>
2021-01-21 18:42:36 +0900

    13305 주유소

--- 13305.py
+++ 13305.py
@@ -5,14 +5,14 @@ len = list(map(int, stdin.readline().split()))
 price = list(map(int, stdin.readline().split()))
 
 result = 0
-start = 0
+minp = price[0]
 
-while(start < n-1):
-    for j in range(start+1, n):
-        if (price[start] < price[j]):
-            result += (price[start]*len[j-1])
-        else:
-            result += (price[start]*len[j-1])
-            start = j
+for i in range(1, n):
+    if minp > price[i]:
+        result += (minp*len[i-1])
+        minp = price[i]
+    else:
+        result += (minp*len[i-1])
+        minp = minp
 
 print(result)
