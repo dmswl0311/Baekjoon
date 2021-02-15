547c26134fb19298320fd2d2aaeaceffad8c5af4
dmswl0311 <dmswl_0311@naver.com>
2021-02-15 22:03:10 +0900

    [DP-타일링] 2133 타일 채우기

--- 2133.py
+++ 2133.py
@@ -4,9 +4,10 @@ d = [0]*(31)
 d[0] = 1
 d[2] = 3
 
-for i in range(3, n+1):
+for i in range(4, n+1):
     d[i] = d[i-2]*3
-    if i % 2 == 0:
-        d[i] += 2*(d[i-4])
+    for j in range(4, i, 2):
+        d[i] += 2*d[i-j]
+    d[i] += 2
 
 print(d[n])
