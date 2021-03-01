c85047c967406813144a2b9601768c738b68f2e2
dmswl0311 <dmswl_0311@naver.com>
2021-03-01 20:53:33 +0900

    11719 그대로 출력하기2

---
+++ 10991.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(1, n+1):
+    if i % 2 != 0:
+        print(" ", end="")
+        print("*", end="")
+        print(" ")
+    else:
+        print("*", end="")
+        print(" ", end="")
+        print("*")
