7870f6e3e53c77eb8326a3d1ad39d8ac3449799a
dmswl0311 <dmswl_0311@naver.com>
2021-03-01 21:05:10 +0900

    10991 별 찍기-16

--- 10991.py
+++ 10991.py
@@ -1,11 +1,7 @@
-n = int(input())
+N = int(input())
 
-for i in range(1, n+1):
-    if i % 2 != 0:
-        print(" ", end="")
-        print("*", end="")
-        print(" ")
-    else:
-        print("*", end="")
-        print(" ", end="")
-        print("*")
+for i in range(1, N+1):
+    print(' ' * (N-i), end='')
+    for j in range(i):
+        print('*', end=' ')
+    print()
