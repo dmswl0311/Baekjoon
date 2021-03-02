cf4e6f36d221c393ef6a7af300cc8305a8c109d4
dmswl0311 <dmswl_0311@naver.com>
2021-03-02 23:03:55 +0900

    [DP] 9095 1,2,3 더하기

---
+++ 9095.py
@@ -0,0 +1,15 @@
+# DP
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    d = [0]*11
+    d[1] = 1
+    d[2] = 2
+    d[3] = 4
+
+    for j in range(4, n+1):
+        d[j] = d[j-1]+d[j-2]+d[j-3]
+
+    print(d[n])
