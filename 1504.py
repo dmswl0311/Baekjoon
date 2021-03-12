3b77f30144fd48f6e1ef16e4caf8241cf46998af
dmswl0311 <dmswl_0311@naver.com>
2021-03-13 00:42:01 +0900

    [dijkstra] 1504 특정한 최단 경로

--- 1504.py
+++ 1504.py
@@ -4,12 +4,12 @@ import sys
 import heapq
 
 input = sys.stdin.readline
-INF = int(1e8)
+INF = int(1e9)
 
 n, e = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
-distance = [INF]*(n+1)
+
 result = 0
 result2 = 0
 
@@ -22,53 +22,28 @@ v1, v2 = map(int, input().split())
 
 
 def dijkstra(start):
+    distance = [INF]*(n+1)
     h = []
     heapq.heappush(h, (0, start))
     distance[start] = 0
     while h:
         dist, now = heapq.heappop(h)
-        if distance[now] < dist:
-            continue
+
         for i in graph[now]:
             cost = i[1]+dist
             if cost < distance[i[0]]:
                 distance[i[0]] = cost
                 heapq.heappush(h, (cost, i[0]))
+    return distance
 
 
-dijkstra(1)
+one = dijkstra(1)
+v1_ = dijkstra(v1)
+v2_ = dijkstra(v2)
 
-result = distance[v1]
-distance = [INF]*(n+1)
-dijkstra(v1)
+cnt = min(one[v1]+v1_[v2]+v2_[n], one[v2]+v2_[v1]+v1_[n])
 
-result += distance[v2]
-distance = [INF]*(n+1)
-dijkstra(v2)
-
-ql = max(distance)
-for i in range(1, len(distance)):
-    if i != v1 and i != v2:
-        ql = min(ql, distance[i])
-
-
-distance = [INF]*(n+1)
-dijkstra(1)
-
-result2 = distance[v2]
-distance = [INF]*(n+1)
-dijkstra(v2)
-
-result2 += distance[v1]
-distance = [INF]*(n+1)
-dijkstra(v1)
-
-ql2 = max(distance)
-for i in range(1, len(distance)):
-    if i != v1 and i != v2:
-        ql2 = min(ql2, distance[i])
-
-if min(ql+result, ql2+result2) >= INF:
+if cnt >= INF:
     print(-1)
 else:
-    print(min(ql+result, ql2+result2))
+    print(cnt)
