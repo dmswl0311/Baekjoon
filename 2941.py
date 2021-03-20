32a18e8588147cfe7f7e137ab81b5d77bcc2c16c
dmswl0311 <dmswl_0311@naver.com>
2021-03-20 23:31:31 +0900

    [문자열] 2941 크로아티아 알파벳

--- 2941.py
+++ 2941.py
@@ -1,15 +1,11 @@
 import sys
+input = sys.stdin.readline
 
-str = sys.stdin.readline()
-print(str)
-cnt = 0
-cnt2 = 0
-list = ["c=", "c-", "dz=", "d", "lj", "nj", "s=", "z="]
-# s1 = str.replace("c=", "č").replace(
-#     "c-", "ć").replace("dz=", "dž").replace("d", "đ").replace("lj", "lj").replace("nj", "nj").replace("s=", "š").replace("z=", "ž")
+s = input().rstrip()
+alpha = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
 
-for i in list:
-    if i in str:
-        cnt = cnt+1
+# 입력한 문자열 s에 alpha에 있는 문자가 포함될 경우 * 로 replace
+for t in alpha:
+    s = s.replace(t, "*")
 
-print(cnt)
+print(len(s))
