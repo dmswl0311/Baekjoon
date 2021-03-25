cf273c1708b0a3a1a602aba30ef9282d8901d3ff
dmswl0311 <dmswl_0311@naver.com>
2021-03-25 23:27:45 +0900

    [삼성기출] 13458 시험 감독

--- 13458.py
+++ 13458.py
@@ -1,3 +1,5 @@
+# 100개의 방, 각 방마다 100개의 사람, 감독관은 각각 1명씩만 볼 수 있을 때 생각해보기
+
 import sys
 input = sys.stdin.readline
 
@@ -10,20 +12,17 @@ cnt = 0
 for i in range(len(a)):
     if a[i]-b >= 0:
         a[i] = a[i]-b
-        cnt += 1
-i = 0
-while(sum(a) > 0):
-    if i == len(a):
-        i = 0
-    if a[i] != 0:
-        if a[i]-c >= 0:
-            a[i] = a[i]-c
+    else:
+        a[i] = 0
+    cnt += 1
+
+    if a[i] > 0:
+        if a[i] <= c:
             cnt += 1
         else:
-            a[i] = 0
-            cnt += 1
-    else:
-        continue
-    i = i+1
+            if(a[i] % c != 0):
+                cnt += (a[i]//c)+1
+            else:
+                cnt += (a[i]//c)
 
 print(cnt)
