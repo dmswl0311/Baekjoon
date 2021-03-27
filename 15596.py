e66e8c1d008485df99aa50a47dc933ee13a98907
dmswl0311 <dmswl_0311@naver.com>
2021-03-27 23:41:20 +0900

    15596 정수 N개의 합

--- 15596.py
+++ 15596.py
@@ -1,15 +1,6 @@
 import sys
-
-a = []
-sum = 0
+input = sys.stdin.readline
 
 
-def solve(int):
-    for i in range(n):
-        a.append(int(sys.stdin.readline()))
-    for i in a:
-        sum += i
-
-
-n = int(sys.stdin.readline())
-print(solve(n))
+def solve(n):
+    return sum(n)
