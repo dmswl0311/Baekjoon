6378a86c873e7f372281d6708acdfe5099124a7a
dmswl0311 <dmswl_0311@naver.com>
2021-04-10 23:42:24 +0900

    [BFS] 1707 이분 그래프

--- 1707.py
+++ 1707.py
@@ -8,13 +8,18 @@ k = int(input())
 def bfs(v):
     q = deque([v])
     visited[v] = True
+    check_list[v] = 1
     while(q):
         x = q.popleft()
-        check_list[x] += 1
         for target in graph[x]:
             if not visited[target]:
                 visited[target] = True
+                check_list[target] = 3-check_list[x]
                 q.append(target)
+            else:
+                if check_list[target] == check_list[x]:
+                    return False
+    return True
 
 
 for _ in range(k):
@@ -23,22 +28,19 @@ for _ in range(k):
     graph = [[] for _ in range(v+1)]
     check_list = [0]*(v+1)
     visited = [False]*(v+1)
+    flag = True
 
     for _ in range(e):
         num, v2 = map(int, input().split())
         graph[num].append(v2)
+        graph[v2].append(num)
 
     for i in range(1, v+1):
         if not visited[i]:
-            bfs(i)
-    print(check_list)
-    flag = 1
-    for i in check_list[1:]:
-        if i > 1:
-            flag = 0
-            break
-
-    if flag == 0:
+            if not bfs(i):
+                flag = False
+                break
+    if flag == False:
         print("NO")
     else:
         print("YES")
