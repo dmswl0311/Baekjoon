45711d0168c8391a1c2b3dbf04de2f4184d72ade
dmswl0311 <dmswl_0311@naver.com>
2021-04-21 23:43:51 +0900

    17144 미세먼지 안녕! - 미세먼지 확산까지(함수)

--- 17144.py
+++ 17144.py
@@ -6,30 +6,39 @@ visited = [[False]*c for _ in range(r)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-# 확산 graph 따로 지정
-graph_dust = [[0]*c for _ in range(r)]
 
-# 미세먼지가 있는 좌표 따로 저장
-dust = []
-for i in range(r):
-    for j in range(c):
-        if graph[i][j] != 0 and graph[i][j] != -1:
-            dust.append((i, j))
-        if graph[i][j] == -1:
-            graph_dust[i][j] = -1
+def dust():
+    # 확산 graph 따로 지정
+    graph_dust = [[0]*c for _ in range(r)]
 
-for x, y in dust:
-    cnt = 0
-    for next in range(4):
-        nx = x+dx[next]
-        ny = y+dy[next]
-        # 인접한 방향에 공기청정기 or 칸이 없을때
-        if nx < 0 or ny < 0 or nx >= r or ny >= c or graph[nx][ny] == -1:
-            continue
-        graph_dust[nx][ny] += graph[x][y]//5
-        cnt += 1
+    # 미세먼지가 있는 좌표 따로 저장 (시간 단축)
+    dust = []
+    for i in range(r):
+        for j in range(c):
+            if graph[i][j] != 0 and graph[i][j] != -1:
+                dust.append((i, j))
+            if graph[i][j] == -1:
+                graph_dust[i][j] = -1
 
-    graph_dust[x][y] += graph[x][y]-((graph[x][y]//5)*cnt)
-    print("x는 {0} y는 {1} cnt는 {2}".format(x, y, cnt))
+    for x, y in dust:
+        cnt = 0
+        for next in range(4):
+            nx = x+dx[next]
+            ny = y+dy[next]
+            # 인접한 방향에 공기청정기 or 칸이 없을때
+            if nx < 0 or ny < 0 or nx >= r or ny >= c or graph[nx][ny] == -1:
+                continue
+            graph_dust[nx][ny] += graph[x][y]//5
+            cnt += 1
 
-print(graph_dust)
+        graph_dust[x][y] += graph[x][y]-((graph[x][y]//5)*cnt)
+        # print("x는 {0} y는 {1} cnt는 {2}".format(x, y, cnt))
+
+    # 원래 그래프에 확산한 graph_dust 옮기기
+    for i in range(r):
+        for j in range(c):
+            graph[i][j] = graph_dust[i][j]
+
+
+dust()
+print(graph)
